c882e55c3f7a83d768395eabb532d163efc1a6c7
mtorres9095 <mtorres9095@gmail.com>
2020-07-14 19:19:05 -0400

    Assignment #4 Completed

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,9 +1,11 @@
 --Part 1
-Id integer;
-Name string;
-EmployerId integer;
---Part 2
+-- Id integer;
+-- Name string;
+-- EmployerId integer;
 
+--Part 2
+-- SELECT * FROM techjobs.employers
+-- WHERE Location = "St. Louis City";
 
 --Part 3
-
+-- 
