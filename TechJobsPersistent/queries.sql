4db7f3129a92813ba424a1f830b61cc371adb2be
mtorres9095 <mtorres9095@gmail.com>
2020-07-15 14:21:15 -0400

    Assignment #4 with Moe's last recommended changes and added Part 3 query

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -8,4 +8,8 @@
 -- WHERE Location = "St. Louis City";
 
 --Part 3
--- 
+-- SELECT jobs.Name, skills.Name, skills.Description
+-- FROM jobskills
+-- INNER JOIN skills ON jobskills.SkillId = skills.Id 
+-- INNER JOIN jobs ON jobs.Id = jobskills.JobId
+-- ORDER BY jobs.Name ASC;
